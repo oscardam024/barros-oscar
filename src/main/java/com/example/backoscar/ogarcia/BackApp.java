67bdd556d544dda919111ebf7af1b8560bbc9724
oscardam024 <dam024.2021@gmail.com>
2022-03-04 12:33:00 +0100

    4

--- src/main/java/com/example/backoscar/ogarcia/BackApp.java
+++ src/main/java/com/example/backoscar/ogarcia/BackApp.java
@@ -1,11 +1,13 @@
 package com.example.backoscar.ogarcia;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BackApp {
-    public BackApp(){
-        throw new Error("Unresolved compilation problems: \n\tThe type java.lang.Object cannot be resolved. It is indirectly referenced from required .class files\n\tThe type java.lang.Class cannot be resolved. It is indirectly referenced from required .class files\n\tThe declared package \"com.juansaldana.barrosback1\" does not match the expected package \"src.main.java.com.juansaldana.barrosback1\"\n\tThe import org cannot be resolved\n\tThe import org cannot be resolved\n\tSpringBootApplication cannot be resolved to a type\n\tImplicit super constructor Object() is undefined for default constructor. Must define an explicit constructor\n\tString cannot be resolved to a type\n\tSpringApplication cannot be resolved\n");
-    }
 
     public static void main(String[] args) {
-        throw new Error("Unresolved compilation problems: \n\tString cannot be resolved to a type\n\tSpringApplication cannot be resolved\n");
+        SpringApplication.run(BackApp.class, args);
     }
+
 }
