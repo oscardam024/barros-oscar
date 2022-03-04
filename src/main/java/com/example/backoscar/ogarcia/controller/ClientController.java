ac6b19fc082658d09a659cdd26987f114ae2fafb
oscardam024 <dam024.2021@gmail.com>
2022-03-04 12:13:48 +0100

    3

--- src/main/java/com/example/backoscar/ogarcia/controller/ClientController.java
+++ src/main/java/com/example/backoscar/ogarcia/controller/ClientController.java
@@ -1,4 +1,34 @@
 package com.example.backoscar.ogarcia.controller;
 
+import com.example.backoscar.ogarcia.model.Client;
+import com.example.backoscar.ogarcia.service.ClientService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@CrossOrigin("*")
+@RestController()
 public class ClientController {
+    @Autowired
+    private ClientService clientService;
+    private final Logger logger = LoggerFactory.getLogger(ClientController.class);
+
+    @GetMapping("/clientes")
+    public ResponseEntity<List<Client>> listaClientes() {
+        logger.info("inicio listaClientes");
+        List<Client> clientes = null;
+
+        clientes = clientService.findAll();
+        logger.info("fin listaClientes");
+        return new ResponseEntity<>(clientes, HttpStatus.OK);
+    }
 }
