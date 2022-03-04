3915c466b593d6be03dbe2416d29cb97fb1a0a29
oscardam024 <dam024.2021@gmail.com>
2022-03-04 12:36:12 +0100

    añadido clase app main  Fixed

--- src/main/java/com/example/backoscar/ogarcia/BackoscarApplication.java
+++ src/main/java/com/example/backoscar/ogarcia/BackoscarApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackApp {
+public class BackoscarApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackApp.class, args);
+        SpringApplication.run(BackoscarApplication.class, args);
     }
 
 }
