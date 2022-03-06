31bfabd21d5bdd6cd4a009a1fbf4b2a757dcae10
oscardam024 <dam024.2021@gmail.com>
2022-03-06 22:28:26 +0100

    ignores test 5

--- src/main/java/com/example/backoscar/ogarcia/SwaggerConfiguration.java
+++ src/main/java/com/example/backoscar/ogarcia/SwaggerConfiguration.java
@@ -13,12 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfiguration {  @Bean
 public Docket api() {
 
-    return new Docket(DocumentationType.SWAGGER_2)
-
-            .select()
-
-            .apis(RequestHandlerSelectors.any())
-
+    return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.example.backoscar.garcia.controller"))
             .paths(PathSelectors.any())
 
             .build();
