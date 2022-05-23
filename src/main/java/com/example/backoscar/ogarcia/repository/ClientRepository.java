c87bee55670e3ba2f1043abb6d9dd26832c53ba9
ogarcia <ogarcia@hiberus.com>
2022-05-23 17:32:11 +0200

    tfg 1.3v

--- src/main/java/com/example/backoscar/ogarcia/repository/ClientRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.backoscar.ogarcia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client,Integer> {
-}
